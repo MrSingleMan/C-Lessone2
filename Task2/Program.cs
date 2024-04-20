d9435446aad542d6b82990ea56546165ca335e2f
Alexander <n00mer00k@gmail.com>
2024-04-20 15:40:58 +0400

    Add Lesson02: Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿int N = 10;
+int[] arr = {1, 3, 5, 7, 2, 3, 9, 0, 5, 2};
+int i = 0;
+
+while (i < arr.Length)
+{
+    if(arr[i] % 2 == 0)
+    {
+    Console.Write($"{arr[i]} ");
+    }
+    i = i + 1;
+}
